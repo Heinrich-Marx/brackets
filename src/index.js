37682996144551a02ea272b685406b636db06fac
heinrich-marx <weriast98@gmail.com>
2021-07-06 20:03:23 +0300

    feat: writed code, but he is working 50/50

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let open = [];
+  let close = new Object();
+  let sumbol;
+  let arr = [];
+  let elem;
+  for (let i = 0; i <bracketsConfig.length; i++) {
+    open.push(bracketsConfig[i][0]);
+    close[bracketsConfig[i][1]] = bracketsConfig[i][0];
+  }
+  for (let i = 0; i<str.length; i++) {
+      sumbol = str[i];
+      if (open.includes(sumbol)) {
+        arr.push(sumbol);
+      }
+      else {
+        if (arr.length === 0) {
+          return false;
+        } 
+      }
+      elem = arr[arr.length - 1];
+      if (close.sumbol == elem) {
+        arr.pop()
+      }
+      else {
+        return false;
+      }
+  }
+  return arr.length === 0;
 }
+
+/*let bracketsConfig = [['(', ')']];
+let str = '((()))()';
+
+let open = [];
+let close = new Object();
+let sumbol;
+let arr = [];
+let elem;
+for (let i = 0; i <bracketsConfig.length; i++) {
+  open.push(bracketsConfig[i][0]);
+  close[bracketsConfig[i][1]] = bracketsConfig[i][0];
+}
+for (let i = 0; i<str.length; i++) {
+    sumbol = str[i];
+    if (open.includes(sumbol)) {
+      arr.push(sumbol);
+    }
+    else {
+      if (arr.length === 0) {
+        return false;
+      } 
+    }
+    elem = arr[arr.length - 1];
+    if (close[sumbol] == elem) {
+      arr.pop()
+    }
+    else {
+      return false;
+    }
+}
+return arr.length === 0; */
